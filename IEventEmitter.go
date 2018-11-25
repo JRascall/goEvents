a0ad965a156740c9f1cb590c03186739eec31132
Jamie.M <j.mascall@hotmail.com>
2018-11-25 16:46:23 +0000

    Figured out how to get the interface to show comments in intellisen

--- IEventEmitter.go
+++ IEventEmitter.go
@@ -2,7 +2,6 @@ package events
 
 // IEventEmitter is the interface that wraps the basic event emitter implementation.
 type IEventEmitter interface {
-	// On test
 	On(name string, handler func(args IEventArgs))
 	Call(name string, args IEventArgs)
 	Delete(name string)
